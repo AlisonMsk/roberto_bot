280ba759bc7d5c65e9af0ad964f371c30f1fccad
Alison Massaki <alison.massaki@going2.com.br>
2022-02-11 01:17:21 -0300

    token

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 const { READY } = require('./events/types.js');
 const { loadAudioStream } = require('./handlers/audioStream.js')
-require('dotenv').config();
 
-const TOKEN = 'OTQxNTI5NzY0OTc0NDUyNzY3.YgXR7A.s0g-q_wZfemhRp1QXNodNdxMLzc';
+const TOKEN = 'OTQxNTI5NzY0OTc0NDUyNzY3.YgXR7A.eVqwDqGrIwmbzwLibiUpCYEja_o';
 console.log(TOKEN)
 
 const bot = new Discord.Client();
