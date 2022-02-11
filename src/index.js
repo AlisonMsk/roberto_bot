063b0bfa554ea0b99131f8988dfadc7b04dad37b
Alison Massaki <alison.massaki@going2.com.br>
2022-02-11 01:41:27 -0300

    token

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const Discord = require('discord.js');
 const { READY } = require('./events/types.js');
 const { loadAudioStream } = require('./handlers/audioStream.js')
+require('dotenv').config();
 
-const token = 'OTQxNTUxNzIyODg1Njg1Mjg4.YgXmXw.8yKqV102OY_IKxwI_ETQlMXKoZ0';
-console.log(token)
+const token = process.env.token;
 
 const bot = new Discord.Client();
-console.log('etapa 1')
+
 bot.on(READY, () => {
     console.log('Bot is running!');
 });
-console.log('etapa 2')
 
 loadAudioStream(bot);
-console.log('etapa 3')
 
 bot.login(token);
-console.log('etapa 4')
